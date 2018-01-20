b7bf77cd8293486b10d3efa261b00fb32a779a27
gibber9809 <gibber9809@users.noreply.github.com>
2018-01-20 12:03:26 -0500

    Fix memory leak in LandingActivity

--- app/src/main/java/com/gunshippenguin/textgame/TextGameLandingActivity.java
+++ app/src/main/java/com/gunshippenguin/textgame/TextGameLandingActivity.java
@@ -11,15 +11,23 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TextGameLandingActivity extends AppCompatActivity {
+    private LandingTextReceiver mReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_game_landing);
-        registerReceiver(new LandingTextReceiver(), new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+
+        if (mReceiver.equals(null)) {
+            mReceiver = new LandingTextReceiver();
+            registerReceiver(mReceiver, new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+        }
     }
 
     protected void dummyHandler(String number) {
@@ -27,31 +35,63 @@ public class TextGameLandingActivity extends AppCompatActivity {
         Toast.makeText(this,number,Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReceiver.equals(null)) {
+            mReceiver = new LandingTextReceiver();
+            registerReceiver(mReceiver, new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+        }
+    }
+
     public class LandingTextReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(final Context context, final Intent intent) {
             final PendingResult pendingResult = goAsync();
-            AsyncTask<Void,Void,SmsMessage[]> asyncHandler = new AsyncTask<Void, Void, SmsMessage[]>() {
+            AsyncTask<Void,Void,List<JSONObject> > asyncHandler = new AsyncTask<Void, Void, List<JSONObject> >() {
                 @Override
-                protected SmsMessage[] doInBackground(Void... voids) {
+                protected List<JSONObject> doInBackground(Void... voids) {
                     if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
                         SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                        List<JSONObject> events = new ArrayList<JSONObject>();
+
                         pendingResult.finish();
-                        return messages;
+                        return events;
                     }
                     pendingResult.finish();
                     return null;
                 }
 
                 @Override
-                protected void onPostExecute(SmsMessage[] results) {
-                    if (results.equals(null))
+                protected void onPostExecute(List<JSONObject> events) {
+                    /*if (results.equals(null))
                         return;
 
                     for (int i = 0; i < results.length; ++i) {
                         TextGameLandingActivity.this.dummyHandler(results[i].getDisplayOriginatingAddress());
-                    }
+                    }*/
                 }
             };
             asyncHandler.execute();
