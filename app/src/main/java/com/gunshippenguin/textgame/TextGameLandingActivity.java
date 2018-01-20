52bf7e5b13c60564b286dc0934ef444af02e103a
gibber9809 <gibber9809@users.noreply.github.com>
2018-01-19 23:58:09 -0500

    Initial commit
    boilerplate to start android text-game project

---
+++ app/src/main/java/com/gunshippenguin/textgame/TextGameLandingActivity.java
@@ -0,0 +1,13 @@
+package com.gunshippenguin.textgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextGameLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_game_landing);
+    }
+}
